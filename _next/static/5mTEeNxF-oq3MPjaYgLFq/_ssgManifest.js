6ee7585684465d6866859b3b81e50ef36f4ad942
zoruhammah-cmyk <zoruhammah-cmyk@users.noreply.github.com>
2026-07-06 20:51:50 +0000

    deploy: a46b1e7049db51a2b835a765c466c31729a082fc

---
+++ _next/static/5mTEeNxF-oq3MPjaYgLFq/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Fareas\u002F[slug]","\u002Fresources\u002F[slug]","\u002Fservices\u002F[slug]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
---
+++ _next/static/chunks/app/not-found-383d523a63698eab.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[345],{6021:(e,s,n)=>{Promise.resolve().then(n.t.bind(n,2619,23)),Promise.resolve().then(n.t.bind(n,1356,23)),Promise.resolve().then(n.bind(n,5287)),Promise.resolve().then(n.bind(n,1398))}},e=>{e.O(0,[987,188,441,255,358],()=>e(e.s=6021)),_N_E=e.O()}]);
